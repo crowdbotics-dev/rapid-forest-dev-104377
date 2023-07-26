10c67d76bb71cbebd3e3c0b56c377d8608d2a906
Crowdbotics <engineering@crowdbotics.com>
2023-07-26 10:20:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rapid-forest-dev-104377",
+  url: "https://rapid-forest-dev-104377.botics.co",
+  api: "https://rapid-forest-dev-104377.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
